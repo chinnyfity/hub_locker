8682c53453d75ab04b476a7f3603664a222f0554
chinny <donchibobo@gmail.com>
2021-07-04 05:53:52 +0100

    template design

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Hub Locker</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+		<link href="assets/css/ionicons.css" rel="stylesheet" type="text/css">
+		<link href="assets/css/jquery.fancybox.css" rel="stylesheet" type="text/css">
+		<!-- REVOLUTION STYLE SHEETS -->
+		<link rel="stylesheet" type="text/css" href="assets/css/settings.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/layers.css">
+		<!--Main Slider-->
+		<link href="assets/css/layers.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="assets/css/owl.carousel.min.css" type="text/css" rel="stylesheet" media="screen">
+		<!-- popup -->
+		<link href="assets/css/magnific-popup.css" rel="stylesheet" type="text/css">
+
+		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/header2.css" rel="stylesheet" type="text/css" />
+		<link href="assets/css/footer2.css" rel="stylesheet" type="text/css" />
+		<link href="assets/css/index2.css" rel="stylesheet" type="text/css" /> <!--main css-->
+		<link href="assets/css/theme-color/default.css" rel="stylesheet" type="text/css" id="theme-color" />
+
+		<link rel='stylesheet' href='assets/css/custom-bootstrap-margin-padding.css' type='text/css' media='all' />
+
+	</head>
+	<body>
+		
+		<div id="preloader">
+			<div class="sk-circle">
+				<div class="sk-circle1 sk-child"></div>
+				<div class="sk-circle2 sk-child"></div>
+				<div class="sk-circle3 sk-child"></div>
+				<div class="sk-circle4 sk-child"></div>
+				<div class="sk-circle5 sk-child"></div>
+				<div class="sk-circle6 sk-child"></div>
+				<div class="sk-circle7 sk-child"></div>
+				<div class="sk-circle8 sk-child"></div>
+				<div class="sk-circle9 sk-child"></div>
+				<div class="sk-circle10 sk-child"></div>
+				<div class="sk-circle11 sk-child"></div>
+				<div class="sk-circle12 sk-child"></div>
+			</div>
+		</div>
+
+		
+		<header id="header" class="header">
+			<div class="nav-wrap">
+				<div class="reletiv_box">
+					<div class="container_">
+						<div class="row">
+
+							<div class="col-xl-3 col-lg-2 col-sm-3 d-flex align-items-center pl-40 pl-sm-20 pl-xs-20" style="backgrounds: pink">
+								<div class="logo">
+									<a href="#"><img src="assets/images/logo.jpg" alt=""></a>
+								</div>
+								<!-- <button id="menu" class="menu d-md-none d-block"></button> -->
+							</div>
+							<button id="menu" class="menu d-md-none d-block"></button>
+
+							<div class="col-xl-9 col-lg-6_ col-sm-9 pl-0 nav-bg" style="backgrounds:red">
+								<nav class="navigation pl-xs-15">
+									<ul>
+										<li><a href="#!">Home</a></li>
+										<li><a href="#!">Find a Locker</a></li>
+										<li><a href="#!">Size Guide</a></li>
+										<li><a href="#!">Locations</a></li>
+										<li><a href="#!">Help Center</a></li>
+										
+										<li class="pl-30 pl-xs-0"><a href="#!">My Account</a></li>
+										<li><a href="tel:080-188-0872">080-188-0872</a></li>
+										<li class="pr-30"><a href="#!"><span class="pay_btn mb-xs--30">PAY YOUR BILL</span></a></li>
+										
+									</ul>
+
+								</nav>
+							</div>
+
+
+
+							<!-- <div class="col-xl-4 col-lg-4 col-sm-4 pr-40 pr-sm-0 nav-bg" style="background:green">
+								<nav class="navigation navigation1">
+									<ul>
+										<li><a href="#!">My Account</a></li>
+										<li><a href="#!">080-188-0872</a></li>
+										<li><a href="#!"><span class="pay_btn">PAY YOUR BILL</span></a></li>
+									</ul>
+								</nav>
+							</div> -->
+
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		
+
+		<div class="main-banner banner_up">
+			<div id="rev_slider_34_1_wrapper" class="rev_slider_wrapper" data-alias="news-gallery34">
+				<!-- START REVOLUTION SLIDER 5.0.7 fullwidth mode -->
+				<div id="rev_slider_34_1" class="rev_slider" data-version="5.0.7">
+					<ul>
+						<!-- SLIDE  -->
+						<li data-index="rs-129"  >
+							<!-- MAIN IMAGE -->
+							<img src="assets/images/banner/slider1.jpg"  alt=""  class="rev-slidebg" >
+							<!-- LAYERS -->
+							<!-- LAYER NR. 2 -->
+							<div class="tp-caption Newspaper-Title tp-resizeme "
+							id="slide-129-layer-1"
+							data-x="['left','left','left','left']" data-hoffset="['20','50','50','10']"
+							data-y="['top','top','150','center']" data-voffset="['130','135','50','0']"
+							data-fontsize="['50','50','50','30']"
+							data-lineheight="['55','55','55','35']"
+							data-width="['600','600','600','458']"
+							data-height="none"
+							data-whitespace="normal"
+							data-transform_idle="o:1;"
+							data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+							data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+							data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+							data-mask_out="x:0;y:0;s:inherit;e:inherit;"
+							data-start="1000"
+							data-splitin="none"
+							data-splitout="none"
+							data-responsive_offset="on" >
+								
+							</div>
+						</li>
+						<!-- SLIDE  -->
+						<li data-index="rs-130" data-title="" data-description="">
+							<!-- MAIN IMAGE -->
+							<img src="assets/images/banner/slider2.jpg"  alt="" class="rev-slidebg">
+							<!-- LAYERS -->
+							<!-- LAYER NR. 2 -->
+							<div class="tp-caption Newspaper-Title   tp-resizeme "
+							id="slide-130-layer-1"
+							data-x="['left','left','left','left']" data-hoffset="['20','50','50','10']"
+							data-y="['top','top','150','center']" data-voffset="['130','135','50','0']"
+							data-fontsize="['50','50','50','30']"
+							data-lineheight="['55','55','55','35']"
+							data-width="['600','600','600','458']"
+							data-height="none"
+							data-whitespace="normal"
+							data-transform_idle="o:1;"
+							data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+							data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+							data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+							data-mask_out="x:0;y:0;s:inherit;e:inherit;"
+							data-start="1000"
+							data-splitin="none"
+							data-splitout="none"
+							data-responsive_offset="on">
+								
+							</div>
+						</li>
+					</ul>
+					<div class="tp-bannertimer tp-bottom"></div>
+				</div>
+			</div>
+
+		</div>
+
+
+		
+		
+
+		<footer class="footer pt-50 mt-50">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-4 footer_logo">
+						<a href=""><img src="assets/images/logo.jpg" alt=""></a>
+						<p class="footer_desc">
+							This is where you will place a short content paragraph about the company. You can also make it more than one line so that it can be seen on google site intro.
+						</p>
+						<ul>
+							<li>
+								<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-lg-3 mt-xs-30 link_footer">
+						<h4>Quick Links</h4>
+						<ul>
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">Find a Locker</a>
+							</li>
+							<li>
+								<a href="#">Size Guide</a>
+							</li>
+							<li>
+								<a href="#">Locations</a>
+							</li>
+							<li>
+								<a href="#">Help Center</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-lg-3 mt-xs-30 link_footer">
+						<h4>Quick Links</h4>
+						<ul>
+							<li>
+								<a href="#">Account</a>
+							</li>
+							<li>
+								<a href="#">Pay Your Bills</a>
+							</li>
+							<li>
+								<a href="#">Privacy & Policy</a>
+							</li>
+							<li>
+								<a href="#">Terms of Use</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-md-6 col-lg-2">
+						
+					</div>
+
+				</div>
+			</div>
+			<div class="bottom-footer text-center">
+				<div class="container">
+					<div class="bor_top clearfix">
+						<p>Copyright © 2021 HubLocker. All rights reserved</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<!-- Site Wraper End -->
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/jquery.easing.js" type="text/javascript"></script>
+
+		<!-- fancybox Js -->
+		<script src="assets/js/jquery.mousewheel-3.0.6.pack.js" type="text/javascript"></script>
+		<script src="assets/js/jquery.fancybox.pack.js" type="text/javascript"></script>
+		<!-- carousel Js -->
+		<script src="assets/js/owl.carousel.min.js" type="text/javascript"></script>
+
+		<!-- masonry,isotope Effect Js -->
+		<script src="assets/js/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+		<script src="assets/js/isotope.pkgd.min.js" type="text/javascript"></script>
+		<script src="assets/js/masonry.pkgd.min.js" type="text/javascript"></script>
+		<script src="assets/js/jquery.appear.js" type="text/javascript"></script>
+
+		<!-- revolution Js -->
+		<script type="text/javascript" src="assets/js/jquery.themepunch.tools.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
+		<script type="text/javascript" src="assets/extensions/revolution.extension.slideanims.min.js"></script>
+		<script type="text/javascript" src="assets/extensions/revolution.extension.layeranimation.min.js"></script>
+		<script type="text/javascript" src="assets/extensions/revolution.extension.navigation.min.js"></script>
+		<script type="text/javascript" src="assets/extensions/revolution.extension.parallax.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery.revolution.js"></script>
+		<!-- popup -->
+		<script src="assets/js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+
+		<!-- custom Js -->
+		<script src="assets/js/custom2.js" type="text/javascript"></script>
+
+
+
+		<!-- <script src="assets/js1/jquery.min.js"></script> -->
+		<!-- <script src="assets/vendor_components/popper/dist/popper.min.js"></script> -->
+		<script src="assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="assets/vendor_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+		<script src="assets/js1/template.js"></script>
+		<script src="assets/js1/demo.js"></script>
+
+		<script src="assets/js1/jquery.dataTables.min.js" type="text/javascript"></script>
+		<script type="text/javascript" src="assets/js1/fnReloadAjax.js"></script>
+		<script src="assets/js1/dataTables.responsive.min.js" type="text/javascript"></script>
+		<script src="assets/js1/dataTables.bootstrap.min.js" type="text/javascript"></script>
+
+
+
+
+
+		<script>
+			var site_urls = $('#txtsite_url').val();
+			var txt_pagename = $('#txtpage_name').val();
+			var txtuid = $('#txtuid').val();
+			var txtuser = "admin";
+
+
+			//alert('site_urls');
+			//alert(txt_pagename);
+			var urls = site_urls+"node/fetch_records/"+txt_pagename+"/"+txtuid+"/"+txtuser+"/";
+			var dataTable = $('#tbl_search').DataTable({
+				"processing": true,
+				"serverSide": true,
+				"pageLength": 25,
+				"order":[],
+				"ajax":{
+					url : urls,
+					type: "post"
+				},
+				"columnDefs":[
+				{
+					"target":[0,3,4],
+					"orderable": false
+				}
+				],
+
+				dom: 'lBfrtip',
+				//buttons: ['excel',  'csv', 'pdf', 'print', 'copy',],
+				"lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
+			});
+
+
+			$('body').on('keyup', '.txtsearchs', function (e) {
+				//$('.dataTables_filter input').trigger('keyup');
+				var txtsearchs = $(this).val();
+				$('.dataTables_filter input').val(txtsearchs);
+				$('.dataTables_filter input').trigger('keyup');
+			});
+		</script>
+
+	</body>
+
+</html>
+
